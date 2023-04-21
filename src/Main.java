06ee307e455fe4eb3d72f51db8a8547dae459afb
PIS4R <240777@edu.p.lodz.pl>
2023-04-21 14:06:31 +0200

    3. Adding the Main class, the display of the phone book etc

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import java.util.*;
+import java.util.TreeMap;
 
 class NrTelefoniczny implements Comparable<NrTelefoniczny> {
     private String codeNumber;
@@ -37,7 +37,7 @@ class NrTelefoniczny implements Comparable<NrTelefoniczny> {
 }
 
 abstract class Wpis {
-    abstract void opis();
+    public abstract void opis();
 }
 
 class Osoba extends Wpis {
@@ -68,12 +68,14 @@ class Osoba extends Wpis {
     public NrTelefoniczny getPhoneNumber() {
         return numberInformation;
     }
+
     public NrTelefoniczny getCodeNumber() {
         return numberInformation;
     }
+
     @Override
-    void opis() {
-        System.out.println(name + " " + surname + " " + adress + " " + "+" + numberInformation.getCodeNumber() + " " + numberInformation.getPhoneNumber());
+    public void opis() {
+        System.out.println("(+" + numberInformation.getCodeNumber() + ") " + numberInformation.getPhoneNumber() + " " + name + " " + surname + " " + adress);
 
     }
 }
@@ -81,45 +83,72 @@ class Osoba extends Wpis {
 class Firma extends Wpis {
     String companyName;
     String companyAdress;
-    NrTelefoniczny companyPhoneNumber;
+    NrTelefoniczny phoneNumber;
 
-    public Firma(String inputedCompanyName, String inputedCompanyAdress, NrTelefoniczny inputedCompanyPhoneNumber ){
+    public Firma(String inputedCompanyName, String inputedCompanyAdress, NrTelefoniczny inputedCompanyPhoneNumber) {
         this.companyName = inputedCompanyName;
         this.companyAdress = inputedCompanyAdress;
-        this.companyPhoneNumber = inputedCompanyPhoneNumber;
+        this.phoneNumber = inputedCompanyPhoneNumber;
     }
 
-    public String getCompanyName(){
+    public String getCompanyName() {
         return companyName;
     }
 
-    public String getCompanyAdress(){
+    public String getCompanyAdress() {
         return companyAdress;
     }
 
-    public NrTelefoniczny getCompanyPhoneNumber(){
-        return companyPhoneNumber;
+    public NrTelefoniczny getPhoneNumber() {
+        return phoneNumber;
     }
 
 
     @Override
-    void opis() {
-        System.out.println(companyName + " " + companyAdress + " " + "+" + companyPhoneNumber.getCodeNumber() + " " + companyPhoneNumber.getPhoneNumber());
+    public void opis() {
+        System.out.println("(+" + phoneNumber.getCodeNumber() + ") " + phoneNumber.getPhoneNumber() + " " + companyName + " " + companyAdress);
     }
 }
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        //TreeMap<NrTelefoniczny, Wpis> phoneBook = new TreeMap<>();
+        TreeMap<NrTelefoniczny, Wpis> phoneBook = new TreeMap<>();
 
         NrTelefoniczny number1 = new NrTelefoniczny("48", "123 456 789");
-        NrTelefoniczny number2 = new NrTelefoniczny("48", "696 969 696");
+        NrTelefoniczny number2 = new NrTelefoniczny("48", "543 678 324");
+        NrTelefoniczny number3 = new NrTelefoniczny("48", "340 586 235");
 
-        Osoba staryDziad = new Osoba("Jaroslaw", "Kaczynski", "Swietokrzyska 17", number1);
-        staryDziad.opis();
-        Firma macDonalds = new Firma("MacDonalds", "Whatever Street 2137", number2);
-        macDonalds.opis();
+        NrTelefoniczny number4 = new NrTelefoniczny("48", "987 654 321");
+        NrTelefoniczny number5 = new NrTelefoniczny("48", "913 721 372");
+        NrTelefoniczny number6 = new NrTelefoniczny("48", "938 518 735");
+
+
+        Osoba person1 = new Osoba("Jaroslaw", "Kaczynski", "Swietokrzyska 17", number1);
+        Osoba person2 = new Osoba("Zbigniew", "Stonoga", "Grota Roweckiego 3", number2);
+        Osoba person3 = new Osoba("Jaroslaw", "Kaczynski", "Wiejska 9", number3);
+
+        //person1.opis();
+        Firma company1 = new Firma("c1", "s1", number4);
+        Firma company2 = new Firma("c2", "s2", number5);
+        Firma company3 = new Firma("c3", "s3", number6);
+
+        //company1.opis();
+        phoneBook.put(person1.getPhoneNumber(), person1);
+        phoneBook.put(person2.getPhoneNumber(), person2);
+        phoneBook.put(person3.getPhoneNumber(), person3);
+        phoneBook.put(company1.getPhoneNumber(), company1);
+        phoneBook.put(company2.getPhoneNumber(), company2);
+        phoneBook.put(company3.getPhoneNumber(), company3);
+
+
+        for (NrTelefoniczny nr : phoneBook.keySet()) {
+            Wpis wpis = phoneBook.get(nr);
+            wpis.opis();
+        }
+
+
     }
 }
 
